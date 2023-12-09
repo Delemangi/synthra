e7bda4e66feef9431ae543c9ffcd6b63f754eb24
Delemangi <milev.stefan@gmail.com>
2023-12-09 23:18:55 +0100

    Setup project

--- backend/app/models/file.py
+++ backend/app/models/file.py
@@ -1,20 +1,24 @@
-# app/models/user.py
-from app.database.base import Base
 import uuid
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from .user import User
+
+from app.database.base import Base
+
 
 class File(Base):
     __tablename__ = "file"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    ID = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
     name = Column(String, nullable=False)
     path = Column(String, nullable=False)
     size = Column(Integer, nullable=False)
     encrypted = Column(Boolean, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
-    user = relationship('User', back_populates='files')
+
     expiration = Column(DateTime, nullable=False)
     timestamp = Column(DateTime, nullable=False)
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.ID"), nullable=False)
+    user = relationship("User", back_populates="files")
--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -1,14 +1,16 @@
-# app/models/user.py
-from app.database.base import Base
 import uuid
+
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+
+from app.database.base import Base
+
 
 class Role(Base):
     __tablename__ = "role"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    ID = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
     name = Column(String, nullable=False)
     quota_size = Column(Integer, nullable=False)
     quota_files = Column(Integer, nullable=True)
