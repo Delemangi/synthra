7ccd1ecc209864f375847a0ef5b36f46a7814130
Delemangi <milev.stefan@gmail.com>
2023-12-16 02:23:55 +0100

    Refactor a few functions

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from .constants import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
 from .models import User
 
-SECRET_KEY = str(os.getenv("JWT_SECRET_KEY"))
+SECRET_KEY = os.getenv("JWT_SECRET_KEY", "SECRET")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -34,15 +34,21 @@ def verify_password(plain_password: str, password: str) -> bool:
 async def authenticate_user(
     username: str, password: str, session: AsyncSession
 ) -> User | None:
-    user = await get_user_by_filter(
-        lambda u: and_(
-            u.username == username, verify_password(password, str(u.password))
-        ),
-        session,
-    )
+    user = await get_user_by_username(username, session)
+
     if not user:
         return None
-    return user
+
+    stored_password = str(user.password)
+    is_password_correct = verify_password(password, stored_password)
+
+    if not is_password_correct:
+        return None
+
+    return await get_user_by_filter(
+        lambda u: and_(u.username == username, u.password == stored_password),
+        session,
+    )
 
 
 def create_access_token(
@@ -64,21 +70,18 @@ async def add_user(user: User, session: AsyncSession) -> None:
     await session.commit()
 
 
-async def get_user_by_filter(user_filter: Callable, session: AsyncSession) -> User:
+async def get_user_by_filter(
+    user_filter: Callable, session: AsyncSession
+) -> User | None:
     users = await session.execute(select(User).filter(lambda: user_filter(User)))
+    return users.scalar_one_or_none()
 
-    return users.first()
 
-
-async def get_user_by_id(user_id: UUID, session: AsyncSession) -> User:
+async def get_user_by_id(user_id: UUID, session: AsyncSession) -> User | None:
     users = await session.execute(select(User).filter(User.id == user_id))
+    return users.scalar_one_or_none()
 
-    return users.first()
 
-
-async def get_user_by_username(username: str, session: AsyncSession) -> User:
-    users = await session.execute(
-        select(User).filter(User.username == username and User.password)
-    )
-
-    return users.first()
+async def get_user_by_username(username: str, session: AsyncSession) -> User | None:
+    users = await session.execute(select(User).filter(User.username == username))
+    return users.scalar_one_or_none()
