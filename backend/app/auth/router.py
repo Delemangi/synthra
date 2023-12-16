476aa97b2c5f3d721021780e9e58a74668029d97
Delemangi <milev.stefan@gmail.com>
2023-12-16 02:07:20 +0100

    Remove useless linter comment

--- backend/app/auth/router.py
+++ backend/app/auth/router.py
@@ -24,7 +24,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(data={"sub": user.username})  # type: ignore
+    access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
