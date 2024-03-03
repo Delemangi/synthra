aaf829c7b77c1478525142ac1e1ef4d62f685153
Alek Jarmov <ajar@bendingspoons.com>
2024-03-04 00:34:17 +0100

    change entrypoint to work with new docker image

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,29 +1,41 @@
 import os
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncEngine
 
 from app.auth.models import Role  # noqa: F401
 from app.file_transfer.models import File, Webhook  # noqa: F401
 from app.models import Base
 
+engine: AsyncEngine | None = None
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-if SQLALCHEMY_DATABASE_URL is None:
-    raise ValueError("DATABASE_URL environment variable is not set")
+def get_engine() -> AsyncEngine:
+    global engine
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-async_session_maker = async_sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
-)
+    if SQLALCHEMY_DATABASE_URL is None:
+        raise ValueError("DATABASE_URL environment variable is not set")
 
+    if engine is None:
+        engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+    return engine
+
+
+async_session_maker: None | async_sessionmaker = None
+def get_session_maker():
+    global async_session_maker
+    if async_session_maker is None:
+        async_session_maker = async_sessionmaker(
+            autocommit=False, autoflush=False, bind=get_engine(), expire_on_commit=False
+        )
+    return async_session_maker
 
 async def initialize_database() -> None:
-    async with engine.begin() as conn:
+    async with get_engine().begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+    async with get_session_maker() as session: # type: ignore
         yield session
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,16 +5,28 @@ from pathlib import Path
 from fastapi import FastAPI
 
 from app.database import initialize_database
-
+import uvicorn
+import time
+import sys
 from .auth.router import router as auth_router
 from .file_transfer.constants import FILE_PATH
 from .file_transfer.router import router as file_router
 
+from app.settings import APISettings
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
     print("Application is starting up")
-    await initialize_database()
+    for i in range(10):
+        try:
+            await initialize_database()
+            break
+        except Exception as ex:
+            print(ex)
+            time.sleep(1)
+        if i == 9:
+            sys.exit()
+
     Path.mkdir(Path(FILE_PATH), exist_ok=True)
 
     yield
@@ -25,12 +37,19 @@ async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
         Path.unlink(f)
 
 
-app = FastAPI(lifespan=lifespan)
+def make_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(auth_router, prefix="/auth")
+    app.include_router(file_router, prefix="/files")
+    @app.get("/")
+    async def root() -> str:
+        return "Hello World"
+    return app
 
-app.include_router(auth_router, prefix="/auth")
-app.include_router(file_router, prefix="/files")
+def run() -> None:
+    app = make_app()
+    settings = APISettings()
+    uvicorn.run(app, host=settings.host, port=settings.port, log_level="info")
 
-
-@app.get("/")
-async def root() -> str:
-    return "Hello World"
+if __name__ == "__main__":
+    run()
