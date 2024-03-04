516da7cdd080b80e8a686248faf9df0758c478c4
Danilo <danilo.najkov@gmail.com>
2024-03-04 19:50:02 +0100

    login and register full

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import initialize_database
 import uvicorn
@@ -42,6 +43,15 @@ async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
 
 def make_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(auth_router, prefix="/auth")
     app.include_router(file_router, prefix="/files")
 
--- frontend/src/routes/auth/register/+page.svelte
+++ frontend/src/routes/auth/register/+page.svelte
@@ -4,40 +4,46 @@
 
   let email = '';
   let password = '';
+  let confirmPassword = '';
 
   async function handleSubmit() {
     try {
-      // Make API call to log in the user
-      const formData = new URLSearchParams();
-      formData.append('username', email);
-      formData.append('password', password);
 
-      const response = await fetch('http://localhost:8002/auth/register', {
+      if (password !== confirmPassword) {
+        alert('Passwords do not match. Please confirm your password.');
+        return;
+      }
+
+      const body = {
+        username: email,
+        password: password
+      }
+      const response = await fetch('http://localhost:8002/auth/register/', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/json'
         },
-        body: formData
+        body: JSON.stringify(body)
       });
 
       if (response.ok) {
-        // Redirect if login successful
-        window.location.href = '/';
+        window.location.href = '/auth/login';
       } else {
-        // Show alert if login failed
-        alert('Login failed. Please check your credentials.');
+        alert('Register failed. Please check your credentials.');
       }
     } catch (error) {
-      console.error('Error logging in:', error);
-      alert('An error occurred while logging in. Please try again later.');
+      console.error('Error registering:', error);
+      alert('An error occurred while registering. Please try again later.');
     }
   }
 </script>
 
 <Header/>
+
 <div style="width: 300px; margin: auto; top: 50%; transform: translate(0, 30vh); border: 1px solid gray; padding:10px; border-radius:5px">
   <TextInput label='Email' bind:value={email} />
   <TextInput type='password' label='Password' bind:value={password} />
+  <TextInput type='password' label='Confirm Password' bind:value={confirmPassword} />
   <br/>
-  <Button on:click={handleSubmit}>Login</Button>
+  <Button on:click={handleSubmit}>Register</Button>
 </div>
