3996e06049e2e5ef44dbb83b5e80a6e49ee03d2c
Ana Marija Atanasovska <amatanasovska@yahoo.com>
2024-03-16 17:17:51 +0100

    style header

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,23 +1,44 @@
 <!-- Header.svelte -->
 <script>
-    import { Switch, Header, Flex, Title } from '@svelteuidev/core';
-
+    import { createStyles, Switch, Header, Flex, Title } from '@svelteuidev/core';
     export let toggleTheme = () => {};
     export let currentTheme = 'light';
+
+    const useStyles = createStyles((theme) => {
+        return {
+          root: {
+            height: 80,
+          },
+          flex: {
+            [`${theme.dark} &`]: {
+              bc: theme.fn.themeColor('dark', 5),
+              color: 'white'
+            },
+            backgroundColor: theme.fn.themeColor('gray', 1),
+            paddingLeft: 20,
+            paddingRight: 20,
+          },
+          leftOptions: {
+            padding: 30
+          }
+        }}
+    );
+
+    $: ({ classes, getStyles } = useStyles());
 </script>
 
 
-<Header slot="header" height="50px">
-  <Flex justify="space-between" align="center" style="height: 100%;">
+<Header class={getStyles()} slot="header" >
+  <Flex class={classes.flex} justify="space-between" align="center" style="height: 100%;">
 
-    <a
+    <a class={classes.leftOptions}
         href="/">
           <Title order={1}>
             Synthra
           </Title>
     </a>
     <Flex justify="space-between">
-      <a
+      <a class={classes.leftOptions}
           href="/auth/login">
             <Title order={3}>
                 Log In
@@ -25,7 +46,9 @@
       </a>
 
       <Switch
+        color="gray"
         on:change={toggleTheme}
+        label={currentTheme == 'dark' ? 'Dark' : 'Light'}
         checked={currentTheme == 'dark'} />
     </Flex>
   </Flex>
