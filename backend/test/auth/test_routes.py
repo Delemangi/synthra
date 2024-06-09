15a0bae2d89a1aaf9a6b82c5be6f1d7096d14675
Delemangi <milev.stefan@gmail.com>
2024-06-09 20:18:37 +0200

    Clean up
    Signed-off-by: Delemangi <milev.stefan@gmail.com>

--- backend/test/auth/test_routes.py
+++ backend/test/auth/test_routes.py
@@ -1,34 +1,40 @@
 import json
-from unittest.mock import AsyncMock, patch, ANY
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from httpx import AsyncClient
 
+from ..constants import HTTP_SUCCESS, HTTP_UNPROCESSABLE_ENTITY
+
 
 @pytest.mark.anyio
-async def test_simple_test(async_client):
+async def test_simple_test(async_client: AsyncClient) -> None:
     response = await async_client.get("/auth/test")
-    assert response.status_code == 200
+
+    assert response.status_code == HTTP_SUCCESS
     assert response.json() == "Hello! The auth router is working."
 
 
 @pytest.mark.anyio
-async def test_logout(async_client: AsyncClient):
+async def test_logout(async_client: AsyncClient) -> None:
     with patch("app.auth.service.remove_token", new_callable=AsyncMock):
-        headers = {"Authorization": f"Bearer test-token"}
+        headers = {"Authorization": "Bearer test-token"}
         response = await async_client.post("/auth/logout", headers=headers)
 
-        assert response.status_code == 200  # Check that the response status is 200
+        assert response.status_code == HTTP_SUCCESS
         assert response.json() == {"message": "Logged out"}
 
 
 @pytest.mark.anyio
-async def test_register_wrong_data(async_client: AsyncClient):
+async def test_register_wrong_data(async_client: AsyncClient) -> None:
     with patch("app.auth.service.create_user", new_callable=AsyncMock) as mock_create_user:
         mock_create_user.return_value = {"username": "test-user"}
 
         form_data = {"username": "test-user", "password": "test-pass"}
         headers = {"Content-Type": "application/json"}
 
-        response = await async_client.post("/auth/register", json=json.dumps(form_data), headers=headers)
-        assert response.status_code == 422
+        response = await async_client.post(
+            "/auth/register", json=json.dumps(form_data), headers=headers
+        )
+
+        assert response.status_code == HTTP_UNPROCESSABLE_ENTITY
--- backend/test/test_main.py
+++ backend/test/test_main.py
@@ -1,7 +1,11 @@
 import pytest
+from httpx import AsyncClient
+
+from .constants import HTTP_SUCCESS
 
 
 @pytest.mark.anyio
-async def test_root(async_client):
+async def test_root(async_client: AsyncClient) -> None:
     response = await async_client.get("/")
-    assert response.status_code == 200
+
+    assert response.status_code == HTTP_SUCCESS
