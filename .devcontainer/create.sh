07a94f2bf2cf21e610ca345ea6f4b569aaad2516
Delemangi <milev.stefan@gmail.com>
2024-06-09 21:36:59 +0200

    Update dev container script
    Signed-off-by: Delemangi <milev.stefan@gmail.com>

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -3,6 +3,6 @@
 sudo mkdir /assets && sudo chown vscode /assets
 
 cd /synthra/backend && poetry install --no-root
-cd /synthra/frontend && npm i
+cd /synthra/backend && poetry run pre-commit install
 
-poetry run pre-commit install
+cd /synthra/frontend && npm i
